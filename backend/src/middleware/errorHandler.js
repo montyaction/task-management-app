16135f31007f5651b78439bdda1ec706d3a5e1b8
montyaction <montyaction@hotmail.com>
2025-09-14 18:59:18 +0530

    refactor(backend): improve error logging in errorHandler middleware
    - updated console.error to log err.stack when availble

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,10 +1,14 @@
+// Handle non-existing routes
 export const notFound = (req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 };
 
+// General error handler
 export const errorHandler = (err, req, res, next) => {
-  console.error(err);
+  console.error(err.stack || err);
+
   const status = err.status || 500;
   const message = err.message || "Internal Server Error";
+
   res.status(status).json({ message });
 };
